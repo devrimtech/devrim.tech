a9855142f7a38f1b99a33fad1d98e4310d9b9956
KaiDevrim <kkgitkraken@devrim.tech>
2020-02-20 09:31:25 -0800

    finished typewriter effect

--- beta/main.js
+++ beta/main.js
@@ -1,5 +1,3 @@
-let replace_text = document.getElementById("front_Replace");
-
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
@@ -8,6 +6,11 @@ const strText = fancytext.textContent;
 const splitText = strText.split("");
 const typeText = ['design websites', 'program games', 'develop apps', 'code software', 'use Arch btw', 'type up my blog', 'make podcasts', 'write stories', 'love to learn'];
 let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+let char = 0;
+let timer = setInterval(onTick, 50);
 
 fancytext.textContent = '';
 
@@ -17,8 +20,31 @@ for (let i = 0; i < splitText.length; i++) {
     fancytext.innerHTML += '<span class="fancySpan">' + splitText[i] + '</span>';
 }
 
-let char = 0;
-let timer = setInterval(onTick, 50);
+(function type (){
+    if (count === typeText.length) {
+        count = 0;
+    }
+    currentText = typeText[count];
+    letter = currentText.slice(0, index++);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length) {
+        count++;
+
+        // if(index > 0) {
+             //currentText.length--;
+             //letter.length--;
+             //currentText--;
+             
+        // }
+        //else {
+            
+        index = 0;
+        console.log(`currentText.length: ${currentText.length} letter.length: ${letter.length} count: ${count} index: ${index} letter: ${letter}`);
+    }
+setTimeout(type, 250);
+
+}())
 
 function onTick() {
     const span = fancytext.querySelectorAll('span')[char];
