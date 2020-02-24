4bcd1e1633e328331be530d1f210bb75a957d088
KaiDevrim <kkgit@devrim.tech>
2020-02-24 09:39:32 -0800

    i no longer hate this

--- beta/main.js
+++ beta/main.js
@@ -1,54 +1,54 @@
-let hamburger = document.querySelector(".hamburger");
-let navLinks = document.querySelector(".nav-links");
-let links = document.querySelectorAll(".nav-links li");
-let fancytext = document.querySelector(".fancyText");
-let strText = fancytext.textContent;
-let splitText = strText.split("");
-const typeText = [
-    "design websites",
-    "program games",
-    "develop apps",
-    "code software",
-    "use Arch btw",
-    "type up my blog",
-    "make podcasts",
-    "write stories",
-    "love to learn"
-];
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+const fancytext = document.querySelector(".fancy-text");
+const strText = fancytext.textContent;
+const splitText = strText.split("");
+const typeText = ['design websites', 'program games', 'develop apps', 'code software', 'use Arch btw', 'type up my blog', 'make podcasts', 'write stories', 'love to learn'];
 let count = 0;
 let index = 0;
-let currentText = "";
-let letter = "";
+let currentText = '';
+let letter = '';
 let char = 0;
 let timer = setInterval(onTick, 50);
 
-fancytext.textContent = "";
+fancytext.textContent = '';
 
 console.log(splitText);
 
 for (let i = 0; i < splitText.length; i++) {
-    fancytext.innerHTML += '<span class="fancySpan">' + splitText[i] + "</span>";
+    fancytext.innerHTML += '<span class="fancySpan">' + splitText[i] + '</span>';
 }
 
-(function type() {
+(function type (){
     if (count === typeText.length) {
         count = 0;
     }
     currentText = typeText[count];
     letter = currentText.slice(0, index++);
 
-    document.querySelector(".typing").textContent = letter;
-    if (letter.length === currentText.length) {
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length) {
         count++;
+
+        // if(index > 0) {
+             //currentText.length--;
+             //letter.length--;
+             //currentText--;
+             
+        // }
+        //else {
+            
         index = 0;
+        console.log(`currentText.length: ${currentText.length} letter.length: ${letter.length} count: ${count} index: ${index} letter: ${letter}`);
     }
 setTimeout(type, 200);
 
 }())
 
 function onTick() {
-    const span = fancytext.querySelectorAll("span")[char];
-    span.classList.add("fade");
+    const span = fancytext.querySelectorAll('span')[char];
+    span.classList.add('fade');
     char++;
 
     if (char == splitText.length) {
@@ -62,9 +62,9 @@ function complete() {
     timer = null;
 }
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
-    });
-});
+    })
+})
