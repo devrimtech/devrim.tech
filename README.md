be003ea05d99bfa1edc03295edc613dd5be7d5b7
Kai Devrim <kai@devrim.tech>
2022-02-07 23:24:46 -0800

    Setup templated source code, now actually get serious

---
+++ README.md
@@ -0,0 +1,5 @@
+# Devrim.Tech
+
+[KaiDevrim's](https://dvr.im/g) portfolio website.
+
+Built off of [@snowpack/app-template-react-typescript](https://github.com/snowpackjs/snowpack/tree/main/create-snowpack-app/app-template-react-typescript).
---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// Note: imports from src/tailwindcss-classnames, not the actual npm package. See compilerOptions.baseUrl in tsconfig.json
+import { classnames } from 'tailwindcss-classnames';
+
+const bg = classnames('bg-ch-pink');
+
+interface AppProps {}
+
+function App({}: AppProps) {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className={classnames(bg, 'text-black', 'p-4', 'rounded', 'mb-4')}>
+          Tailwind, with built-in tooling for custom configs
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
