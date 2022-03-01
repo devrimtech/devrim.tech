beac75899f01aef5628f41dcca204d35b9ca659c
Kai Devrim <kai@devrim.tech>
2022-02-28 21:15:10 -0800

    Finish projects section
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,11 +2,14 @@ import About from './about/about';
 import Header from './header/header';
 export const Home = () => {
 	return (
-		<main className="home">
-			<Header />
-			<About />
-			{/* 			<Webring /> */}
-		</main>
+		<>
+			<main className="home">
+				<Header />
+				<About />
+				{/* 			<Webring /> */}
+			</main>
+			<br /><br /><br />
+		</>
 	)
 }
 
