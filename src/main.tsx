bda9cf48ce96ce876d1f2835db948a8c0de972b4
Kai Devrim <kai@devrim.tech>
2022-03-04 20:15:51 -0800

    Fully remove Service Worker
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,14 @@ import { render } from 'preact';
 import { App } from './App';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.ready.then(registration => {
+		registration.unregister();
+		console.log("Worker Removed");
+	});
+}
+
+else {
+	console.log("No service worker to remove");
+}
 render(<App />, document.getElementById('app')!)
