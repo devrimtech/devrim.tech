eb54d3bdd89e8e1fa545fa128e369f89dc1cff39
Kai Devrim <kai@devrim.tech>
2022-03-04 20:50:28 -0800

    Fix name in webring
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- src/components/webring/webring.tsx
+++ src/components/webring/webring.tsx
@@ -1,7 +1,7 @@
 export const Webring = () => {
 	return (
 		<div className="webring content" style="overflow: hidden;">
-			<iframe src="https://overengineering.kognise.dev/embed/KaiDevrim" width="100%" height="100" scrolling="no" style="overflow: hidden; user-select: none;" frameBorder="0"></iframe>
+			<iframe src="https://overengineering.kognise.dev/embed/kaidevrim" width="100%" height="100" scrolling="no" style="overflow: hidden; user-select: none;" frameBorder="0"></iframe>
 		</div>
 	)
 }
