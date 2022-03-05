bbd74698e33c5fd720fb0a26847a79714ecbc590
Kai Devrim <kai@devrim.tech>
2022-03-04 20:08:37 -0800

    Remove PWA Bug
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,4 @@ import { render } from 'preact';
 import { App } from './App';
 import './index.css';
 
-if (process.env.NODE_ENV === 'production') {
-	// cache all assets if browser supports serviceworker
-	if ('serviceWorker' in navigator && location.protocol === 'https:') {
-		navigator.serviceWorker.register('/sw.js');
-	}
-}
-
-
 render(<App />, document.getElementById('app')!)
