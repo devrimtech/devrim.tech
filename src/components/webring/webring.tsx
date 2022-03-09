2644a9862f4e9eaaf1d704ab85d7bc17dcb4e71a
Kai Devrim <kai@devrim.tech>
2022-03-09 13:38:26 -0800

    Switch to Next.js
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- src/components/webring/webring.tsx
+++
@@ -1,9 +0,0 @@
-export const Webring = () => {
-	return (
-		<div className="webring content">
-			<iframe src="https://overengineering.kognise.dev/embed/KaiDevrim" width="100%" height="100" style="user-select: none;" frameBorder="0"></iframe>
-		</div>
-	)
-}
-
-export default Webring;
--- src/pages/blog/blog.tsx
+++
@@ -1,5 +0,0 @@
-export default function Blog(props: any) {
-	return (
-		<div> Hello Earth </div>
-	)
-}
