f3d3f0a3eec5b47b538545c18d7d6d2e79b17629
Kai Devrim <kai@devrim.tech>
2022-03-11 14:09:39 -0800

    Update Uses Page
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -25,27 +25,36 @@ function UsesPage() {
       <p> The technology and equipment I use on a daily basis </p>
       <ul className="uses-list">
         <h3> My Main Desk </h3>
-        <li> M1 MacBook Air™️ 16GB 512GB SSD </li>
+        <li> M1 MacBook Air 16GB 512GB SSD </li>
         <li> 500GB SSD for Backups</li>
-        <li> Elgato™️ HD60S+ for Console Switching </li>
+        <li> Elgato HD60S+ for Console Switching </li>
         <li> MX Master 2S Wireless Mouse </li>
-        <li> Keychron™️ K8 Wireless Keyboard with White LED </li>
-        <li> 34 inch, 1440p, 75hz, Asus™️ Monitor </li>
-        <li> Modded 1st gen PS4 with a 1TB HDD for extra games </li>
+        <li> Keychron K8 Wireless Keyboard with White LED </li>
+        <li> 34 inch, 1440p, 75hz, Asus Monitor </li>
+        <li>Modded 1st gen. PlayStation 4 with a 1TB HDD for extra games</li>
         <li>
-          {" "}
-          Modded 1st gen Nintendo Switch with two 128GB microSD and a 256GB
-          microSD{" "}
+          Modded 1st gen. Nintendo Switch️ with two 128GB microSD and a 256GB
+          microSD
         </li>
         <li> Nintendo Switch Pro Controller (Xenoblade Edition) </li>
         <h3> Things Around my Room </h3>
-        <li> iPad Pro 1st gen with 1st gen Apple Pencil </li>
+        <li> iPad Pro 1st gen. with 1st gen. Apple Pencil </li>
         <li> FitBit Versa 2 </li>
-        <li>Google Home Mini</li>
+        <li> Google Home Mini </li>
+        <h3> Software </h3>
+        <li> Microsoft Edge Beta (Main Browser) </li>
+        <li> Microsoft Edge (School Browser) </li>
+        <li> Nova (Text Editor/IDE)n</li>
+        <li>Tower (Git Client)</li>
+        <li>Postbox (Mail Client)</li>
+        <li>Plex iPad Version (Catalyst)</li>
+        <li> Kopia (Backup Software) </li>
+        <li> Shottr (Screenshot Tool) </li>
+        <li> BitWarden (Password Manager) </li>
       </ul>
       <p> Last updated 2022-03-11 </p>
       <p>
-        Credits to{" "}
+        Credits to
         <Link href="https://github.com/wesbos/awesome-uses/"> Wes Bos </Link>
       </p>
     </div>
