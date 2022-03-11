0e56d290c2cc42eaa8724527e1c11f14429f41b9
Kai Devrim <kai@devrim.tech>
2022-03-11 09:23:24 -0800

    Add Now to NavBar and Mobile Optimizations
    Signed-off-by: Kai Devrim <kai@devrim.tech>

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 export const Navbar = () => {
-	return (
-		<header className="nav-header">
-			<nav className="nav-nav">
-				<Link href="/">Kai Devrim</Link>
-				<Link href="/blog">Blog</Link>
-				<Link href="/resume">Résumé</Link>
-				<Link href="/now">Now</Link>
-				<Link href="/contact">Contact</Link>
-			</nav>
-		</header>
-	)
-}
+  return (
+    <header className="nav-header">
+      <nav className="nav-nav">
+        <Link href="/">Kai Devrim</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/resume">Résumé</Link>
+        <Link href="/now">Now</Link>
+        <Link href="/uses">Uses</Link>
+        <Link href="/contact">Contact</Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
