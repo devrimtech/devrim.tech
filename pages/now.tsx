8223de16199a41edfb296b71c581a79325e7aed3
Kai Devrim <kai@devrim.tech>
2025-06-12 19:20:11 -0700

    Update now.tsx to 2025-06-12

--- pages/now.tsx
+++ pages/now.tsx
@@ -18,12 +18,13 @@ function NowPage() {
       <h1> Now </h1>
       <h3> Here is what I am up to nowadays </h3>
       <ul className="now-list">
-        <h3> Starting up college at Seattle University: </h3>
-        <li> I am currently at my new home, Seattle Washington! </li>
+        <h3> Continuing my education at Bellevue College: </h3>
+        <li> I am currently at my favorite place, Seattle Washington! </li>
         <h3> Learning about Computers: </h3>
         <li> Programming various projects. <Link href="https://github.com/KaiDevrim/"> What I am working on currently </Link></li>
-        <li> In the IEEE club. </li>
-        <li> Reading books on Math and Circutry</li>
+        <li> Constantly going to Raves and Concerts. </li>
+        <li> Just hanging out with friends whenever I can. </li>
+        <li> Reading books. </li>
         <li> Participating in local events. </li>
         <h3> Entertaining Myself: </h3>
         <li> Watching a lot on my Plex Server. </li>
@@ -32,7 +33,7 @@ function NowPage() {
         </li>
         <li> Having fun with my dog, Mocha. </li>
       </ul>
-      <p> Last updated 2023-11-24 </p>
+      <p> Last updated 2025-06-12 </p>
       <p>
         Credits to <Link href="https://nownownow.com"> Derek Sivers </Link>
       </p>
